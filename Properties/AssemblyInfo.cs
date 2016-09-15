7845ed6fa171a479bc654a1530a777fff5f277b3
Artyom Silivonchik <mr.hant@gmail.com>
2016-09-15 09:30:46 +0300

    Create new VS project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationPractice")]
+[assembly: AssemblyProduct("AutomationPractice")]
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a92084ac-3990-464a-bb1d-9c311a0ee5ce")]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
